6aa428347f6afe8188dc979b27c8461b65909254
Dhana bayya <krishnabayya@MacBookAir.lan>
2025-03-06 20:11:43 -0600

    Added backend

---
+++ backend/src/main/java/com/launchcode/dama_devs/DamaDevsApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.dama_devs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamaDevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamaDevsApplication.class, args);
+	}
+
+}
