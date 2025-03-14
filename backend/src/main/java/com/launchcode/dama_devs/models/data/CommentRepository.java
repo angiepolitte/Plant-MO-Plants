94ebdf964c67206f25edee6af95c45f33f7213f4
Allysonhd <166686180+allysonhd@users.noreply.github.com>
2025-03-14 13:10:15 -0500

    fixed missing <Comment, Integer> from CommentRepository

--- backend/src/main/java/com/launchcode/dama_devs/models/data/CommentRepository.java
+++ backend/src/main/java/com/launchcode/dama_devs/models/data/CommentRepository.java
@@ -1,8 +1,9 @@
 package com.launchcode.dama_devs.models.data;
 
+import com.launchcode.dama_devs.models.Comment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository extends CrudRepository {
+public interface CommentRepository extends CrudRepository<Comment, Integer> {
 }
