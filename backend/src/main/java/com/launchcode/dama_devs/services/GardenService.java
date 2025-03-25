0f4c5425faf1cdd56d97142c521e955e5c80cf67
melicu <melcusumano@gmail.com>
2025-03-25 05:41:09 -0500

    Updated Garden Service

--- backend/src/main/java/com/launchcode/dama_devs/services/GardenService.java
+++ backend/src/main/java/com/launchcode/dama_devs/services/GardenService.java
@@ -1,37 +1,39 @@
 package com.launchcode.dama_devs.services;
 
+import com.launchcode.dama_devs.models.data.GardenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class GardenService {
 
-    @Autowired
-    private GardenRepository gardenRepository;
+    //Creates an instance of the variable
+    private final GardenRepository gardenRepository;
 
     @Autowired
     public GardenService (GardenRepository gardenRepository) {
         this.gardenRepository = gardenRepository;
     }
 
-    //Get all user's gardens in dashboard
+    //Get all user's gardens in main dashboard
     public List<Garden> getAllGardens() {
         return gardenRepository.findAll();
     }
 
-    //Get a specific user garden
-    public Optional<Garden> getGardenById(ID id) {
-        return gardenRepository.findById(id)
+    //Get a specific user garden (display in garden detail page)
+    public Optional<Garden> getGardenById(Integer id) {
+        return gardenRepository.findById(id);
     }
 
-    //Update a user garden
+    //Checks to see if a garden exists. If it does not, creates a new garden. If it does, updates the existing garden.
     public Garden saveGarden(Garden garden) {
         return gardenRepository.save(garden);
     }
 
     //Delete a user garden
-    public void deleteGarden(ID id) {
+    public void deleteGarden(Integer id) {
         gardenRepository.deleteById(id);
     }
 }
