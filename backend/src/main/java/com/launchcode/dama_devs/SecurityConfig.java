9252439ca312fd1be98e3ad757cb9b50acd0b187
Dhana bayya <krishnabayya@MacBookAir.lan>
2025-03-27 18:54:05 -0500

    created service classes for user & implemented user details stored in DB

--- backend/src/main/java/com/launchcode/dama_devs/SecurityConfig.java
+++ backend/src/main/java/com/launchcode/dama_devs/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.launchcode.dama_devs;
 
+import com.launchcode.dama_devs.models.AppRole;
+import com.launchcode.dama_devs.models.Role;
+import com.launchcode.dama_devs.models.User;
+import com.launchcode.dama_devs.models.data.RoleRepository;
+import com.launchcode.dama_devs.models.data.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -28,23 +36,65 @@ public class SecurityConfig {
         return http.build();
     }
 
+    //    @Bean
+//    public UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        if (!manager.userExists("user1")) {
+//            manager.createUser(User.withUsername("user1")
+//                    .password("{noop}password")
+//                    .roles("USER")
+//                    .build()
+//            );
+//        }
+//        if (!manager.userExists("admin")) {
+//            manager.createUser(User.withUsername("admin")
+//                    .password("{noop}password1")
+//                    .roles("ADMIN")
+//                    .build()
+//            );
+//        }
+//        return manager;
+//    }
+
     @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        if (!manager.userExists("user1")) {
-            manager.createUser(User.withUsername("user1")
-                    .password("{noop}password")
-                    .roles("USER")
-                    .build()
-            );
-        }
-        if (!manager.userExists("admin")) {
-            manager.createUser(User.withUsername("admin")
-                    .password("{noop}password1")
-                    .roles("ADMIN")
-                    .build()
-            );
-        }
-        return manager;
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CommandLineRunner initData(RoleRepository roleRepository,
+                                      UserRepository userRepository,
+                                      PasswordEncoder passwordEncoder) {
+        return args -> {
+            Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                    .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_USER)));
+
+            Role adminRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+                    .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_ADMIN)));
+
+            if (!userRepository.existsByUsername("user1")) {
+                User user1 = new User("user1", "user1@example.com",
+                        passwordEncoder.encode("password1"));
+                user1.setAccountNonLocked(false);
+                user1.setAccountNonExpired(true);
+                user1.setCredentialsNonExpired(true);
+                user1.setEnabled(true);
+                user1.setSignUpMethod("email");
+                user1.setRole(userRole);
+                userRepository.save(user1);
+            }
+
+            if (!userRepository.existsByUsername("admin")) {
+                User admin = new User("admin", "admin@example.com",
+                        passwordEncoder.encode("adminPass"));
+                admin.setAccountNonLocked(true);
+                admin.setAccountNonExpired(true);
+                admin.setCredentialsNonExpired(true);
+                admin.setEnabled(true);
+                admin.setSignUpMethod("email");
+                admin.setRole(adminRole);
+                userRepository.save(admin);
+            }
+        };
     }
 }
--- backend/src/main/java/com/launchcode/dama_devs/models/User.java
+++ backend/src/main/java/com/launchcode/dama_devs/models/User.java
@@ -5,19 +5,13 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Entity
-@Getter
+@Data
 @NoArgsConstructor
 @Table(name = "users",
         uniqueConstraints = {
