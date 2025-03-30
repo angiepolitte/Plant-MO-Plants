5507c57ba8e8d0033b7b4eb1c7770fea8198ce5a
Allysonhd <166686180+allysonhd@users.noreply.github.com>
2025-03-30 09:55:05 -0500

    created plant filtering service for garden matching plants and updated plant controller

--- backend/src/main/java/com/launchcode/dama_devs/controllers/PlantController.java
+++ backend/src/main/java/com/launchcode/dama_devs/controllers/PlantController.java
@@ -1,11 +1,15 @@
 package com.launchcode.dama_devs.controllers;
 
+import com.launchcode.dama_devs.models.Plant;
 import com.launchcode.dama_devs.models.data.PlantRepository;
+import com.launchcode.dama_devs.services.PlantFilteringService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
@@ -14,4 +18,13 @@ public class PlantController {
 
     @Autowired
     private PlantRepository plantRepository;
+
+    @Autowired
+    private PlantFilteringService plantFilteringService;
+
+    @GetMapping("/{gardenId}/add-plants")
+    public ResponseEntity<List<Plant>> getMatchingGardenPlants(@PathVariable Integer gardenId) {
+        List<Plant> matchingGardenPlants = plantFilteringService.filterPlantsByGardenFields(gardenId);
+        return ResponseEntity.status(HttpStatus.OK).body(matchingGardenPlants);
+    }
 }
