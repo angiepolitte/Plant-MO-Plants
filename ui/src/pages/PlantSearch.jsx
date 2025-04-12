4f0018918996a4d4488d55612f1f36f9092db2d4
Allysonhd <166686180+allysonhd@users.noreply.github.com>
2025-04-11 22:00:54 -0500

    beginning work on plant search page

--- ui/src/pages/PlantSearch.jsx
+++ ui/src/pages/PlantSearch.jsx
@@ -1,19 +1,72 @@
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { Grid2 } from "@mui/material";
+import PlantCard from "../reusable-code/PlantCard";
+import "../custom-css/PlantSearch.css";
 
 function PlantSearch() {
+  const { gardenId } = useParams(); //grabs the gardenId from the URL.
+  const [plants, setPlants] = useState(null);
   const navigate = useNavigate();
 
   const handleNavigateGardenDetails = () => {
-    navigate("/garden-details"); // Make sure this path matches your route for ExampleOne
+    navigate("/garden-details");
   };
 
+  useEffect(() => {
+    let ignore = false;
+    async function fetchPlants() {
+      const response = await fetch(
+        `http://localhost:8080/plant/${gardenId}/search-plants`
+      );
+      const data = await response.json();
+
+      if (!ignore) {
+        setPlants(data);
+      }
+    }
+    fetchPlants();
+    return () => {
+      ignore = true;
+    };
+  }, [gardenId]);
+
+  //this maps through the fetched list of plants and
+  //assigns each plantObject to the "plant" prop to pass into the PlantCard function
+  const plantList = plants.map((plantObject) => (
+    <PlantCard key={plantObject.id} plant={plantObject}></PlantCard>
+  ));
+
+  if (!plants) {
+    return (
+      <div className="title">Please wait while your plants are picked...</div>
+    );
+  }
+
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Plant Results</h1>
-      <h2>and move on to garden details</h2>
-      <button onClick={handleNavigateGardenDetails}>
-        Go to Garden Details
+    <div className="container">
+      <h2 className="title">Find plants for your garden!</h2>
+      <button className="garden-button" onClick={handleNavigateGardenDetails}>
+        VIEW YOUR GARDEN
       </button>
+      <div>
+        {plantList}
+        <Grid2 container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid2 xs={12} sm={6} md={4}>
+            <PlantCard />
+          </Grid2>
+          <Grid2 xs={12} sm={6} md={4}>
+            <PlantCard />
+          </Grid2>
+          <Grid2 xs={12} sm={6} md={4}>
+            <PlantCard />
+          </Grid2>
+          <Grid2 xs={12} sm={6} md={4}>
+            <PlantCard />
+          </Grid2>
+        </Grid2>
+      </div>
     </div>
   );
 }
--- ui/src/reusable-code/PlantCard.jsx
+++ ui/src/reusable-code/PlantCard.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import "../custom-css/PlantCard.css";
+import { Link } from "react-router-dom";
 
-const PlantCard = () => {
+function PlantCard({ plant }) {
   return (
     <div className="plantCard">
-      <img
-        className="plantCard-image"
-        src="https://www.rareroots.com/cdn/shop/files/HeucheraGreenSpice.jpg?v=1703167230&width=1920"
-        alt="Picture of green coral bells"
-      />
-      <h2 className="plantCard-title">Coral Bells</h2>
-      <br></br>
-      <button className="plantCard-button">ADD TO GARDEN</button>
+      <div>
+        <Link to={`/plant-details/${plant.id}`}>
+          <img
+            className="plantCard-image"
+            src={plant.plantImagePath}
+            alt={`Picture of ${plant.commonName}`}
+          />
+          <h2 className="plantCard-title">{plant.commonName}</h2>
+          <br></br>
+        </Link>
+      </div>
+      <div>
+        <button className="plantCard-button">ADD TO GARDEN</button>
+      </div>
     </div>
   );
-};
+}
 
 export default PlantCard;
